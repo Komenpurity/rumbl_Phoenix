c9917c52d2f75988d02a2090e259d6c5e24a4c02
Komenpurity <jepkorirpurity8@gmail.com>
2022-05-22 10:10:14 +0300

    added a user route

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -17,6 +17,8 @@ defmodule RumblWeb.Router do
   scope "/", RumblWeb do
     pipe_through :browser
 
+    get "/users", UserController, :index
+    get "/users/:id", UserController, :index 
     get "/", PageController, :index
   end
 
